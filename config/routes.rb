e7be522a8b2da56afc9d5c1dfcfe697d2e3b4b80
Adam Xiao <42500628+adam-xiao@users.noreply.github.com>
2020-09-02 16:55:52 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :media
   resources :users
+
+  post '/login', to: 'auth#login'
+
+  get "/auto_login", to: "auth#auto_login"
+
+  get "/getmedia", to: "users#selfMedia"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
